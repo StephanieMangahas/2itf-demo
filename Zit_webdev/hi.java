fafc742328ac12a7e887f5cb3ea97259f04776a7
Abigail Aldave <maryabigail.aldave.cics@ust.edu.ph>
2023-02-10 20:29:11 +0800

    added in hi.java and added count.java

--- Zit_webdev/hi.java
+++ Zit_webdev/hi.java
@@ -1,7 +1,8 @@
 public class Main {
 
   public static void main(String[] args) {
-  	System.out.println("stan newjeans"); 
+  	System.out.println("stan newjeans");
+	System.out.println("stan twice"); 
   }
   
 }
