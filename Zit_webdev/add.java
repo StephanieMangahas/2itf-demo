4acb28bcb943717b07e511d14d401132930ae1a6
Mark Francis Padua <markfrancis.padua.cics@ust.edu.ph>
2023-02-10 18:58:30 +0800

    added a new comment

--- Zit_webdev/add.java
+++ Zit_webdev/add.java
@@ -5,7 +5,7 @@ class Main {
     int first = 10;
     int second = 20;
 
-    int sum = first + second;
+    int sum = first + second;//Addition ito 
     System.out.println(first + " + " + second + " = "  + sum);
   }
 }
